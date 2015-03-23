86b873ffc7d65c7723d7411708a2612129b88585
Iwasaki Yudai <yudai@arielworks.com>
2015-03-23 10:44:52 -0700

    Parse CamelCase words

--- .emacs.d/inits/50_ispell.el
+++ .emacs.d/inits/50_ispell.el
@@ -1,2 +1,4 @@
+(require 'ispell)
+(setq ispell-extra-args '("-a" "-m" "--run-together" "--run-together-limit=5" "--run-together-min=2" "--sug-mode=ultra"))
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-mode)
