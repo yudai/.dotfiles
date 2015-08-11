816e1bb6975ae45511b93e712cda78a7ebe5d46b
Iwasaki Yudai <yudai@arielworks.com>
2015-08-10 17:43:34 -0700

    Move around windows with vi keymap

--- .emacs.d/inits/60_global_keymap.el
+++ .emacs.d/inits/60_global_keymap.el
@@ -6,11 +6,18 @@
 (define-key global-map (kbd "\C-x 6 6") 'toggle-truncate-lines)
 
 (define-key global-map (kbd "C-M-o") 'other-window)
+
 (define-key global-map (kbd "C-M-k") 'windmove-up)
 (define-key global-map (kbd "C-M-j") 'windmove-down)
 (define-key global-map (kbd "C-M-l") 'windmove-right)
 (define-key global-map (kbd "C-M-h") 'windmove-left)
 
+(define-key global-map (kbd "C-x C-k") 'windmove-up)
+(define-key global-map (kbd "C-x C-j") 'windmove-down)
+(define-key global-map (kbd "C-x C-l") 'windmove-right)
+(define-key global-map (kbd "C-x C-h") 'windmove-left)
+
+(define-key global-map (kbd "C-x M-1") 'balance-windows)
+
 (define-key global-map (kbd "M-o") 'bs-cycle-next)
 (define-key global-map (kbd "M-p") 'bs-cycle-previous)
-
