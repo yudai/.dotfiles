a73e75424e924f30e90813f5f0ac13acb236efe1
Iwasaki Yudai <yudai@arielworks.com>
2015-08-12 20:11:16 -0700

    Disable Helm on Neotree and close Neotree with c-g

--- .emacs.d/inits/50_neotree.el
+++ .emacs.d/inits/50_neotree.el
@@ -1,7 +1,8 @@
+(setq neo-keymap-style 'concise)
+(setq neo-banner-message nil)
 (setq neo-show-hidden-files t)
 (setq neo-create-file-auto-open t)
 (setq neo-persist-show t)
-(setq neo-keymap-style 'concise)
 (setq neo-smart-open t)
 (setq neo-vc-integration '(face char))
 
@@ -12,3 +13,11 @@
                         (lambda () (setq neo-persist-show t))))
 
 (define-key global-map (kbd "C-x \"") 'neotree-toggle)
+
+(add-hook 'neotree-mode-hook
+          (lambda ()
+            (define-key neotree-mode-map (kbd "C-g") 'neotree-hide)))
+
+; Disable helm
+(add-to-list 'helm-completing-read-handlers-alist '(neo-buffer--rename-node . nil))
+(add-to-list 'helm-completing-read-handlers-alist '(neotree-create-node . nil))
