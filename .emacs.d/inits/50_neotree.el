17d133b2eb2f6226f41ed696bce7587bd72ba360
Iwasaki Yudai <yudai@arielworks.com>
2016-09-26 18:59:37 -0700

    Disable neo-persist-show

--- .emacs.d/inits/50_neotree.el
+++ .emacs.d/inits/50_neotree.el
@@ -2,15 +2,9 @@
 (setq neo-banner-message nil)
 (setq neo-show-hidden-files t)
 (setq neo-create-file-auto-open t)
-(setq neo-persist-show t)
+(setq neo-persist-show nil)
 (setq neo-smart-open t)
 (setq neo-vc-integration '(face char))
-
-(when neo-persist-show
-  (add-hook 'popwin:before-popup-hook
-            (lambda () (setq neo-persist-show nil)))
-  (add-hook 'popwin:after-popup-hook
-                        (lambda () (setq neo-persist-show t))))
 
 (define-key global-map (kbd "C-x \"") 'neotree-toggle)
 
