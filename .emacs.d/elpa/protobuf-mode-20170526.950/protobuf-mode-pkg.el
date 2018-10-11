9aa7a2cf3886692ae86b442ec0d175b2fe2e2a35
Iwasaki Yudai <yudai@arielworks.com>
2018-10-11 16:07:16 -0700

    Update emacs plugins

---
+++ .emacs.d/elpa/protobuf-mode-20170526.950/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20170526.950" "major mode for editing protocol buffers." 'nil :commit "6a4fec616ec4b20f54d5fb530808b855cb664390" :keywords '("google" "protobuf" "languages"))
