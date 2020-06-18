21606f62607e99413d8d2e883a8c7e4ab6c6d206
Iwasaki Yudai <yudai.iwasaki@ntti3.com>
2020-06-18 15:38:46 -0700

    Update emacs config

--- .emacs.d/inits/50_tide.el
+++ .emacs.d/inits/50_tide.el
@@ -15,5 +15,16 @@
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
-
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
+(setq-default typescript-indent-level 2)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
